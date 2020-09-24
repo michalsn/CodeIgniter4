b6992b269d269a26beac179bbfa941e1ec4842b3
michalsn <michal@sniatala.pl>
2020-09-24 07:50:45 +0200

    action sqlsrv update

--- tests/_github/Database.php
+++ tests/_github/Database.php
@@ -42,6 +42,26 @@ $dbconfig = [
 	   'port'     => 5432,
    ],
 
+   'sqlsrv'   => [
+	   'DSN'      => '',
+	   'hostname' => 'localhost',
+	   'username' => 'mssql',
+	   'password' => 'mssql',
+	   'database' => 'mssql',
+	   'DBDriver' => 'Sqlrv',
+	   'DBPrefix' => 'db_',
+	   'pConnect' => false,
+	   'DBDebug'  => (ENVIRONMENT !== 'production'),
+	   'charset'  => 'utf8',
+	   'DBCollat' => 'utf8_general_ci',
+	   'swapPre'  => '',
+	   'encrypt'  => false,
+	   'compress' => false,
+	   'strictOn' => false,
+	   'failover' => [],
+	   'port'     => 1433,
+   ],
+
    'sqlite'   => [
 	   'DSN'      => '',
 	   'hostname' => 'localhost',
