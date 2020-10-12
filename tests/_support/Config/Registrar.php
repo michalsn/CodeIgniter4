76ce2034cf562757d551fdc0da6e5408b8fb1965
michalsn <michal@sniatala.pl>
2020-10-12 13:51:09 +0200

    connect

--- tests/_support/Config/Registrar.php
+++ tests/_support/Config/Registrar.php
@@ -72,7 +72,7 @@ class Registrar
 			'port'     => 3306,
 		],
 		'OCI8'    => [
-			'DSN'      => '',
+			'DSN'      => 'system/oracle@//localhost:1521/xe',
 			'hostname' => 'localhost',
 			'username' => 'system',
 			'password' => 'oracle',
