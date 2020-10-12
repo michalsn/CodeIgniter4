c4d2c6c9e1a5cecfbc84a4f9950c925ef22a0d1c
michalsn <michal@sniatala.pl>
2020-10-12 10:44:46 +0200

    copyrights

--- system/Database/OCI8/Connection.php
+++ system/Database/OCI8/Connection.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CodeIgniter
  *
@@ -7,6 +8,7 @@
  * This content is released under the MIT License (MIT)
  *
  * Copyright (c) 2014-2019 British Columbia Institute of Technology
+ * Copyright (c) 2019-2020 CodeIgniter Foundation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -28,7 +30,7 @@
  *
  * @package    CodeIgniter
  * @author     CodeIgniter Dev Team
- * @copyright  2014-2019 British Columbia Institute of Technology (https://bcit.ca/)
+ * @copyright  2019-2020 CodeIgniter Foundation
  * @license    https://opensource.org/licenses/MIT	MIT License
  * @link       https://codeigniter.com
  * @since      Version 4.0.0
@@ -43,7 +45,7 @@ use CodeIgniter\Database\Exceptions\DatabaseException;
 use stdClass;
 
 /**
- * Connection for Postgre
+ * Connection for OCI8
  */
 class Connection extends BaseConnection implements ConnectionInterface
 {
