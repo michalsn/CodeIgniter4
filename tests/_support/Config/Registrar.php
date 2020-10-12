1487752a3f409808764b903717efcaa55060b888
michalsn <michal@sniatala.pl>
2020-10-12 10:17:23 +0200

    config

--- tests/_support/Config/Registrar.php
+++ tests/_support/Config/Registrar.php
@@ -71,6 +71,25 @@ class Registrar
 			'failover' => [],
 			'port'     => 3306,
 		],
+		'OCI8'    => [
+			'DSN'      => '',
+			'hostname' => 'localhost',
+			'username' => 'system',
+			'password' => 'oracle',
+			'database' => 'test',
+			'DBDriver' => 'Postgre',
+			'DBPrefix' => 'db_',
+			'pConnect' => false,
+			'DBDebug'  => (ENVIRONMENT !== 'production'),
+			'charset'  => 'utf8',
+			'DBCollat' => 'utf8_general_ci',
+			'swapPre'  => '',
+			'encrypt'  => false,
+			'compress' => false,
+			'strictOn' => false,
+			'failover' => [],
+			'port'     => 1521,
+		],
 	];
 
 	/**
