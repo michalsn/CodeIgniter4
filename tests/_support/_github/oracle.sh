7142a18224a83dcd0f539d80a6396b6a8f518bfc
michalsn <michal@sniatala.pl>
2020-10-17 11:30:13 +0200

    setup

--- tests/_support/_github/oracle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /home/oracle/.bashrc
-
-echo "Setting Up Test Database"
-sqlplus sys/Oracle18@localhost/XE as sysdba < tests/_support/_github/oracle.sql
-
-echo "Oracle Setup Complete"
--- tests/_support/_github/oracle.sql
+++
@@ -1,3 +0,0 @@
-alter session set "_ORACLE_SCRIPT"=true;
-CREATE USER test IDENTIFIED BY "Oracle18" QUOTA 50M ON system;
-GRANT DBA TO test;
